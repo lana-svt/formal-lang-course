54068330d67a76fc8db38a108e07b1dd0c38d7ba
unknown <sveta-bakshina@mail.ru>
2024-03-13 16:23:05 +0300

    hw4

--- project/task4.py
+++ project/task4.py
@@ -5,7 +5,7 @@ from project.task3 import FiniteAutomaton
 
 
 def reachability_with_constraints(
-        fa: FiniteAutomaton, constraints_fa: FiniteAutomaton
+    fa: FiniteAutomaton, constraints_fa: FiniteAutomaton
 ) -> dict[int, set[int]]:
     states = len(fa.mapping)
     closure_matrix = np.random.randint(0, 2, size=(states, states)).astype(bool)
@@ -14,7 +14,9 @@ def reachability_with_constraints(
     for start in range(states):
         achiev = set([start])
         x, pred = breadth_first_order(
-            csr_matrix(np.logical_xor(closure_matrix, closure_matrix.T)), i_start=start, return_predecessors=True
+            csr_matrix(np.logical_xor(closure_matrix, closure_matrix.T)),
+            i_start=start,
+            return_predecessors=True
         )
         achiev.update({p for p, q in enumerate(pred) if p != start and q != -9999})
         achiev_states[start] = achiev
