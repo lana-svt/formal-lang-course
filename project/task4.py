f1a77f78ec5d3272ff5e1ec8df9216576a0c4ab7
lana-svt <sveta-bakshina@mail.ru>
2024-06-08 00:43:12 +0300

    fix

--- project/task4.py
+++ project/task4.py
@@ -1,40 +1,55 @@
 from scipy.sparse import dok_matrix, block_diag
+
 from project.task3 import FiniteAutomaton
 
 
 def reachability_with_constraints(
-    fa: FiniteAutomaton, constraints_fa: FiniteAutomaton
+    automaton: FiniteAutomaton, constraint_automaton: FiniteAutomaton
 ) -> dict[int, set[int]]:
-    m, n = constraints_fa.size(), fa.size()
-    result = {s: set() for s in fa.start_states}
+    m, n = constraint_automaton.size(), automaton.size()
 
-    common_labels = fa.labels() & constraints_fa.labels()
+    def get_front(start_state_index):
+        front = dok_matrix((m, m + n), dtype=bool)
+        for i in constraint_automaton.start_indices():
+            front[i, i] = True
+        for i in range(m):
+            front[i, start_state_index + m] = True
+        return front
+
+    def diag(mat):
+        result = dok_matrix(mat.shape, dtype=bool)
+        for i in range(mat.shape[0]):
+            for j in range(mat.shape[0]):
+                if mat[j, i]:
+                    result[i] += mat[j]
+        return result
+
+    common_labels = automaton.labels() & constraint_automaton.labels()
+    result = {s: set() for s in automaton.start_states}
     adj = {
-        label: block_diag((constraints_fa.transitions[label], fa.transitions[label]))
+        label: block_diag(
+            (constraint_automaton.transitions[label], automaton.transitions[label])
+        )
         for label in common_labels
     }
 
-    for v in fa.start_states:
-        front = dok_matrix((m, m + n), dtype=bool)
-        for i in constraints_fa.start_indices():
-            front[i, i] = True
-        for i in range(m):
-            front[i, v + m] = True
-
-        last_front_hash = -1
+    for v in automaton.start_indices():
+        front = get_front(v)
+        last_hash = -1
         for _ in range(m * n):
             front = sum(
                 [dok_matrix((m, m + n), dtype=bool)]
-                + [front @ adj[label] for label in common_labels]
+                + [diag(front @ adj[label]) for label in common_labels]
             )
-
-            front_indices = front[:, m:].nonzero()
-            for a, b in zip(front_indices[0], front_indices[1]):
-                if a in constraints_fa.final_indices() and b in fa.final_indices():
+            fr = front[:, m:].nonzero()
+            for a, b in zip(fr[0], fr[1]):
+                if (
+                    a in constraint_automaton.final_indices()
+                    and b in automaton.final_indices()
+                ):
                     result[v].add(b)
-            current_front_hash = hash(str(front_indices))
-            if current_front_hash == last_front_hash:
-                break
-            last_front_hash = current_front_hash
+                if hash(str(fr)) == last_hash:
+                    break
+                last_hash = hash(str(fr))
 
     return result
